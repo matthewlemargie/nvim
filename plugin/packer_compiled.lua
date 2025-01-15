2379bd229fb3a60d54ee33ef4c8018072186dc31
Matthew Lemargie <matthewlemargie03@gmail.com>
2025-01-15 00:03:10 -0800

    add vim.fugitive and mappings for it

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -164,6 +164,11 @@ _G.packer_plugins = {
     path = "/home/dmac/.local/share/nvim/site/pack/packer/start/undotree",
     url = "https://github.com/mbbill/undotree"
   },
+  ["vim-fugitive"] = {
+    loaded = true,
+    path = "/home/dmac/.local/share/nvim/site/pack/packer/start/vim-fugitive",
+    url = "https://github.com/tpope/vim-fugitive"
+  },
   ["wrapping.nvim"] = {
     config = { "\27LJ\2\n6\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\rwrapping\frequire\0" },
     loaded = true,
